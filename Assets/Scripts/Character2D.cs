89a0605824ef481f01d816922857bdfce3c8fbb0
Guillermo Candia Huerta <guillermo.candia@gmail.com>
2017-11-13 06:00:19 -0300

    2DPlayer: idle, walk, run

---
+++ Assets/Scripts/Character2D.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.Animations;
+
+namespace Platformer2D
+{
+    [RequireComponent(typeof(Animator))]
+    [RequireComponent(typeof(Rigidbody2D))]
+    public class Character2D : MonoBehaviour
+    {
+        [Header("Configuration")]
+        [SerializeField] private float speed_walk = 40.0f;
+        [SerializeField] private float speed_run = 100.0f;
+
+        private Rigidbody2D rg2D;
+        private Animator anim;
+        private bool facingRight = true;
+
+        [HideInInspector] public float move;
+        [HideInInspector] public bool running;
+        private bool grounded;
+
+        // aux
+        private Vector3 velocity;
+
+        void Awake() 
+        {
+            rg2D = GetComponent<Rigidbody2D> ();
+            anim = GetComponent<Animator>();
+        }
+
+        void FixedUpdate()
+        {
+            grounded = true;
+
+            if (grounded)
+            {
+                Move();
+            }
+
+            if ((move > 0 && !facingRight) || (move < 0 && facingRight))
+            {
+                Flip();
+            }
+        }
+
+        private void Move ()
+        {
+            velocity = Vector3.zero;
+            velocity.y = rg2D.velocity.y;
+            velocity.x = move * (running ? speed_run : speed_walk) * Time.fixedDeltaTime;
+
+            rg2D.velocity = velocity;
+
+            anim.SetFloat("Speed", Mathf.Abs(move));
+            anim.SetBool("Running", running);
+        }
+
+
+        private void Flip()
+        {
+            facingRight = !facingRight;
+            transform.Rotate(0, 180, 0);
+        }
+    }
+}
---
+++ Assets/Scripts/Character2DUserControl.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Platformer2D
+{
+    [RequireComponent(typeof(Character2D))]
+    public class Character2DUserControl : MonoBehaviour
+    {
+        private Character2D character2D;
+       
+        private float move;
+        private bool running;
+
+        private void Awake()
+        {
+            character2D = GetComponent<Character2D>();
+        }
+            
+        private void Update()
+        {
+            
+
+        }
+            
+        private void FixedUpdate()
+        {
+            move = Input.GetAxis("Horizontal");
+            running = Input.GetButton("Fire3");
+
+            character2D.running = running;
+            character2D.move = move;
+        }
+    }
+}
